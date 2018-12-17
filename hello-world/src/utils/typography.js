8bdc7d2fa2cfe66e861068c9a2209262f46a115d
Henrique <fale.henrique@gmail.com>
2018-12-17 20:31:46 -0200

    A simple page with gatsby

---
+++ hello-world/src/utils/typography.js
@@ -0,0 +1,13 @@
+import Typography from "typography"
+
+// import bootstrapTheme from "typography-theme-bootstrap"
+import lawtonTheme from "typography-theme-lawton"
+const typography = new Typography(lawtonTheme)
+
+// const typography = new Typography(
+//     {
+//         baseFontSize: "20px"
+//     }
+// )
+
+export default typography
